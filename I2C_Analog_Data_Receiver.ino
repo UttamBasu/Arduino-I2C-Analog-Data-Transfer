68ee7156f1d83b536f3f1cd1a59b9f9fdd99906f
Uttam Basu <59360744+UttamBasu@users.noreply.github.com>
2021-08-26 23:26:57 +0530

    Add files via upload

---
+++ I2C_Analog_Data_Receiver.ino
@@ -0,0 +1,56 @@
+/*
+  I2C Slave Control Demo
+  It will receive the data from I2C Communication
+  https://github.com/UttamBasu
+*/
+
+// Include Arduino Wire library for I2C
+#include <Wire.h>
+
+// Define Slave I2C Address
+#define SLAVE_ADDR 9
+
+// Define LED Pin
+int LED = 13;
+
+// Variable for received data
+int rd;
+
+// Variable for blink rate
+int br;
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+
+  // Initialize I2C communications as Slave
+  Wire.begin(SLAVE_ADDR);
+
+  // Function to run when data received from master
+  Wire.onReceive(receiveEvent);
+
+  // Setup Serial Monitor
+  Serial.begin(9600);
+  Serial.println("I2C Slave Demonstration");
+}
+
+void receiveEvent()
+{
+  // read one character from the I2C
+  rd = Wire.read();
+  // Print value of incoming data
+  Serial.println(rd);
+}
+
+void loop()
+{
+  delay(50);
+
+  // Calculate blink value
+  br = map(rd, 1, 255, 100, 2000);
+
+  digitalWrite(LED, HIGH);
+  delay(br);
+  digitalWrite(LED, LOW);
+  delay(br);
+}
---
+++ I2C_Analog_Data_Transmitter.ino
@@ -0,0 +1,35 @@
+/*
+  I2C Master Control Demo
+  https://github.com/UttamBasu
+*/
+
+// Include Arduino Wire library for I2C
+#include <Wire.h>
+
+// Define Slave I2C Address
+#define SLAVE_ADDR 9
+
+// Analog pin for potentiometer
+int analogPin = 0;
+// Integer to hold potentiometer value
+int val = 0;
+
+void setup()
+{
+  // Initialize I2C communications as Master
+  Wire.begin();
+}
+
+void loop()
+{
+  delay(50);
+
+  // Read pot value
+  // Map to range of 1-255 for flash rate
+  val = map(analogRead(analogPin), 0, 1023, 255, 1);
+
+  // Write a charatre to the Slave
+  Wire.beginTransmission(SLAVE_ADDR);
+  Wire.write(val);
+  Wire.endTransmission();
+}
